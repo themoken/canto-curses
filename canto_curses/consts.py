f75f1de59da55a853147a3f7e90ed01bb4434197
Jack Miller <jack@codezen.org>
2010-08-31 20:06:40 -0500

    More cruft.
    Signed-off-by: Jack Miller <jack@codezen.org>

--- canto_curses/consts.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#Canto-curses - ncurses RSS reader
-#   Copyright (C) 2010 Jack Miller <jack@codezen.org>
-#
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License version 2 as 
-#   published by the Free Software Foundation.
-
-GUI_EXIT = -1
