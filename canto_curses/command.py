db284f5470a5b9e276af2e78029aa2361c41009b
Jack Miller <jack@codezen.org>
2014-07-09 22:38:27 -0500

    Remove dead arg_transform reference

--- canto_curses/command.py
+++ canto_curses/command.py
@@ -6,7 +6,7 @@
 #   it under the terms of the GNU General Public License version 2 as 
 #   published by the Free Software Foundation.
 
-from canto_next.plugins import PluginHandler, Plugin, add_arg_transform
+from canto_next.plugins import PluginHandler, Plugin
 
 import traceback
 import logging
