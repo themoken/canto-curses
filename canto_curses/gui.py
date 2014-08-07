53b19407b71744b4262827afc043a325aaf1c485
Jack Miller <jack@codezen.org>
2014-08-06 20:40:19 -0500

    Update copyright date in headers

--- canto_curses/gui.py
+++ canto_curses/gui.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 #Canto-curses - ncurses RSS reader
-#   Copyright (C) 2010 Jack Miller <jack@codezen.org>
+#   Copyright (C) 2014 Jack Miller <jack@codezen.org>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License version 2 as 
 #   published by the Free Software Foundation.
-
-COMPATIBLE_VERSION = 0.4
 
 from canto_next.plugins import Plugin
 from canto_next.format import escsplit
--- canto_curses/screen.py
+++ canto_curses/screen.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #Canto-curses - ncurses RSS reader
-#   Copyright (C) 2010 Jack Miller <jack@codezen.org>
+#   Copyright (C) 2014 Jack Miller <jack@codezen.org>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License version 2 as 
