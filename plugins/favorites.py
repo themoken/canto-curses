1e3527a78d3e9a2e0d1c3951a6e48edae77fd891
Jack Miller <jack@codezen.org>
2014-08-09 20:59:53 -0500

    Add favorites plugin
    This is more a proof on concept than a fully implemented plugin.
    Implements 'favorite' and binds it to *. Also updates the story format
    to add a * in front of the title of favorited items.

---
+++ plugins/favorites.py
@@ -0,0 +1,31 @@
+# Favorite Plugin
+# by Jack Miller
+# v1.0
+
+# This plugin allows certain items to be tagged as 'user:favorite' and then a
+# custom style to be applied to those items.
+
+from canto_curses.taglist import TagListPlugin
+from canto_curses.story import StoryPlugin
+from canto_curses.command import register_commands
+
+class StoryFavorite(StoryPlugin):
+    def __init__(self, story):
+        self.plugin_attrs = {}
+        story.pre_format += "%?{'user:favorite' in ut}(*:)"
+
+class TagListFavorite(TagListPlugin):
+    def __init__(self, taglist):
+        self.plugin_attrs = {}
+
+        cmds = {
+            "favorite" : (self.cmd_favorite, ["item-list"], "Favorite items."),
+        }
+        register_commands(taglist, cmds)
+
+        self.taglist = taglist
+
+        taglist.bind('*', 'favorite')
+
+    def cmd_favorite(self, items):
+        self.taglist.cmd_tag_item('%favorite', items)
