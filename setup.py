ed76dd03cb1f3a65267294ff8763bc70e7b8a4e7
Jack Miller <jack@codezen.org>
2014-09-13 12:19:52 -0500

    Start installing plugins to [prefix]/lib/canto/plugins
    Not sure if this is where they'll end up, but I'm thinking it should be
    a common directory since they're installed into a common directory
    (.canto-ng/plugins).

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.command.install_data import install_data
 from distutils.command.build_py import build_py
 from distutils.core import setup, Extension
 import subprocess
+import glob
 import os
 
 string_version = "0.9.0-alpha3"
@@ -48,7 +49,8 @@ setup(name='Canto-curses',
               library_dirs = ["/usr/local/lib", "/opt/local/lib"],
               include_dirs = ["/usr/local/include", "/opt/local/include"])],
       scripts=['bin/canto-curses'],
-      data_files = [("share/man/man1/", ["man/canto-curses.1"])],
+      data_files = [("share/man/man1/", ["man/canto-curses.1"]),
+                    ("lib/canto/plugins/", glob.glob('plugins/*.py'))],
       cmdclass = {  'install_data' : canto_curses_install_data,
                     'build_py' : canto_curses_build_py},
      )
