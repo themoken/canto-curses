bf33c761d2c41eff7f3ba8db984d50800fd1acd0
Jack Miller <jack@codezen.org>
2015-06-13 14:30:37 -0500

    Include default tag theme in theme-default.py

--- plugins/theme-default.py
+++ plugins/theme-default.py
@@ -7,10 +7,11 @@ from canto_next.plugins import check_program
 check_program("canto-curses")
 
 from canto_curses.story import StoryPlugin
+from canto_curses.tag import TagPlugin
 from canto_curses.theme import prep_for_display
 from canto_curses.color import cc
 
-class CantoThemeDefault(StoryPlugin):
+class CantoThemeStoryDefault(StoryPlugin):
     def __init__(self, story):
         self.story = story
         self.plugin_attrs = { "eval" : self.eval }
@@ -44,3 +45,41 @@ class CantoThemeDefault(StoryPlugin):
             s += cc.end("unread")
 
         return s
+
+class CantoThemeTagDefault(TagPlugin):
+    def __init__(self, tag):
+        self.tag = tag
+        self.plugin_attrs = { "eval" : self.eval }
+
+    def eval(self):
+        tag = self.tag
+
+        # Make sure to strip out the category from category:name
+        str_tag = tag.tag.split(':', 1)[1]
+
+        unread = len([s for s in tag\
+                if "canto-state" not in s.content or\
+                "read" not in s.content["canto-state"]])
+
+        s = ""
+
+        if tag.selected:
+            s += cc("selected")
+
+        if tag.collapsed:
+            s += "[+]"
+        else:
+            s += "[-]"
+
+        s += " " + str_tag + " "
+
+        s += "[" + cc("unread") + str(unread) + cc.end("unread") + "]"
+
+        if tag.updates_pending:
+            s += " [" + cc("pending") + str(tag.updates_pending) + cc.end("pending") + "]"
+
+        if tag.selected:
+            s += cc.end("selected")
+
+        return s
+
