97b91fb4615fcc7d0c124b237e3d9fac61f30cc2
Jack Miller <jack@codezen.org>
2015-07-08 12:05:09 -0500

    cleantitle.py: option to forcibly remove anything that looks like HTML

--- plugins/cleantitle.py
+++ plugins/cleantitle.py
@@ -19,11 +19,34 @@ replacements = [
     ("<nobr />", ""),
 ]
 
+# Also included, the option to forcibly remove anything that looks like HTML.
+# Useful for cleaning horribly formatted feeds, but possibly destructive of
+# good content wrapped in <>. Set this to True to enable.
+
+NO_HTML_EVER = False
+
 from canto_next.plugins import check_program
 
 check_program("canto-curses")
 
 from canto_curses.story import StoryPlugin
+
+# From kjellgren (canto-curses issue #18)
+
+def remove_html_markup(s):
+    tag = False
+    quote = False
+    out = ""
+    for c in s:
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif (c == '"' or c == "'") and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+    return out
 
 class CleanTitle(StoryPlugin):
     def __init__(self, story):
@@ -35,5 +58,8 @@ class CleanTitle(StoryPlugin):
 
         for o,n in replacements:
             t = t.replace(o, n)
+
+        if NO_HTML_EVER:
+            t = remove_html_markup(t)
 
         self.story.content["title"] = t
